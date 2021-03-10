4cb5de9fb59d55b1ff83f612666732d9af1f063b
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2021-03-09 21:33:05 -0300

    add todos

--- src/index.js
+++ src/index.js
@@ -10,35 +10,65 @@ app.use(express.json());
 
 const users = [];
 
+// Middlewares
+
 function checksExistsUserAccount(request, response, next) {
-  const { username } = request.body;
+  const { username } = request.headers;
 
-  const user = users.find((users) => user.username === username);
+  const user = users.find((user) => username === user.username);
 
-  if(!user){
-     return response.status(400).json({error: "User not found"});
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" });
   }
 
   request.user = user;
 
   return next();
 }
+function checksExistsUserTodo(request, response, next) {
+  const { id } = request.params;
+  const { user } = request;
 
+  const todos = user.todos;
+
+  const todo = todos.find(todo => id === todo.id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found!" });
+  }
+
+  request.todo = todo;
+
+  return next();
+}
+
+
+// Routes
 app.post('/users', (request, response) => {
   const { name, username } = request.body;
 
-  users.push({
+  const userFound = users.find((user) => username === user.username);
+
+  if (userFound) {
+    return response.status(400).json({ error: "User already exists!" });
+  }
+
+  const user = {
     id: uuidv4(),
     name,
     username,
     todos: []
-  });
+  }
+
+  users.push(user);
 
   return response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.status(200).json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
